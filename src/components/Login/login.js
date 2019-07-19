5cbce76964263497f6cb81204a90823c27e7ee1c
Saharcasm95 <nooresahar95@gmail.com>
2019-07-19 16:56:57 +1000

    login view created

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Button, Modal, InputGroup, FormControl } from 'react-bootstrap';
+import { Route } from 'react-router';
+import { FaEnvelope, FaExclamationCircle } from 'react-icons/fa';
 import './login.scss'
 
 
@@ -29,31 +32,49 @@ class Login extends React.Component{
 
     render(){
         return (
-            <div className="input-group mb-3">
-            <input 
-                type="text" 
-                className="form-control" 
-                placeholder="Email"
-                value={this.state.email}
-                onChange={(e) => this.setState({ email: e.target.value })}
-            />
-            <input 
-                type="text" 
-                className="form-control" 
-                placeholder="Password"
-                value={this.state.password}
-                onChange={(e) => this.setState({ password: e.target.value })}
-            />
+            <div id="login-wrapper" >
+              <Modal.Dialog centered>
+                <Modal.Header className="text-center">
+                  <Modal.Title>Login</Modal.Title>
+                </Modal.Header>
 
-            <div className="input-group-append">
-                <button 
-                type="button" 
-                className="btn btn-info"
-                onClick={this.submit}>
-                Login
-                </button>
+                <Modal.Body>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="email-addon"><FaEnvelope /></InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                      value={this.state.email}
+                      onChange={(e) => this.setState({ email: e.target.value })}
+                      type="text"
+                      placeholder="Email Address"
+                      aria-label="Email Address"
+                      aria-describedby="email-addon"
+                    />
+                  </InputGroup>
+
+                  <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="password-addon"><FaExclamationCircle /></InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                      value={this.state.password}
+                      onChange={(e) => this.setState({ password: e.target.value })}
+                      type="text"
+                      placeholder="Password"
+                      aria-label="Password"
+                      aria-describedby="password-addon"
+                    />
+                  </InputGroup>
+
+                </Modal.Body>
+
+                <Modal.Footer className="text-center">
+                  <Button className="login-button" variant="primary" onClick={this.submit}>Log in</Button>
+                </Modal.Footer>
+              </Modal.Dialog>
+
             </div>
-          </div>
         );
     }
 }
